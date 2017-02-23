5e5c4c41eb6095915a0c639222df7c81f8206dd0
Allan Barsanulfo <Allan Barsanulfo>
2017-02-23 11:06:24 -0300

    Inclusão das classes e alteração do templante.

---
+++ app/ValidaMigracaoV5/src/main/java/Controller/ValidaEnderecoPessoa.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author allanfraga
+ */
+public class ValidaEnderecoPessoa implements InterfaceMigracao {
+    private ArrayList<String> log = new ArrayList<String>();
+    private Integer excelRowNumber = null;
+    private String numDocumento = null;
+    private String logradouro = null;
+    private String numero = null;
+    private String complemento = null;
+    private String bairro = null;
+    private String cidade = null;
+    private String uf = null;
+
+    public String getNumDocumento() {
+        return numDocumento;
+    }
+
+    public void setNumDocumento(String numDocumento) {
+        this.numDocumento = numDocumento;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+    
+    @Override
+    public void addLog(String log) {
+        this.log.add(log);
+    }
+
+    @Override
+    public ArrayList<String> getLog() {
+        return log;
+    }
+    
+    @Override
+    public void setExcelRowNumber(Integer row) {
+        excelRowNumber = row;
+    }
+
+    @Override
+    public Integer getExcelRowNumber() {
+         return excelRowNumber;
+    }
+    
+    public String toString()
+    {
+        // concatena todas as propriedades em uma string (Somente os campos que existem no template)
+        return  numDocumento + logradouro + numero + complemento + bairro + cidade + uf;
+    }
+}
